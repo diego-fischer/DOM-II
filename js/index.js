4ea50afe9085fade562309ac2ba44b888b5fca9a
Diego Fischer <diego@carupi.com>
2021-08-11 14:58:02 -0300

    advancing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+//   * `mouseover`
+//   * `keydown`
+//   * `wheel`
+//   * `load`
+//   * `focus`
+//   * `resize`
+//   * `scroll`
+//   * `select`
+//   * `dblclick`
+//   * `drag / drop`
+
+console.log('teste')
+
+const aTags = document.querySelectorAll('a.nav-link')
+
+aTags.forEach((e) => {
+  e.addEventListener('click', (e) => {
+    console.log('CURRENT TARGET', e.target)
+    e.target.classList.add('shadedTag')
+  })
+})
